ab98171fedde027912db36f82c2aa2c813b10ec5
maru44 <takumaru22244@gmail.com>
2021-08-20 21:49:50 +0900

    meisouchu

--- morse.go
+++ morse.go
@@ -1,21 +1,121 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"morse/config"
 	"morse/pkg/keyboard"
+	"os"
+	"time"
 )
 
 func main() {
-	// keyboard.InputEventOrIntervalFail()
 
-	// go func() {
+	var (
+	// ch_inp = inputer(os.Stdin)
+	// to     = time.After(20 * time.Second)
+	)
+
+	// result := ""
+
+	// keyInput:
 	// 	for {
-	// 		fmt.Print(keyboard.InputEventOrInterval())
+	// 		over := time.After(config.TYPING_INTERVAL * time.Millisecond)
+	// 		select {
+	// 		case v := <-ch_inp:
+	// 			str := keyboard.ConvertInputCode(v)
+	// 			fmt.Print(str)
+	// 			result += str
+	// 			if str == config.QUIT_LETTER {
+	// 				break keyInput
+	// 			}
+	// 		case <-over:
+	// 			str := config.INTERVAL_LETTER
+	// 			fmt.Print(str)
+	// 			result += str
+	// 		}
 	// 	}
-	// }()
-	// time.Sleep(11 * time.Second)
-	resultString := ""
+	// 	fmt.Println(result)
+	fmt.Print(oneWord())
+}
 
-	ret := keyboard.MyGetKey(resultString)
-	fmt.Printf("result: %s", ret)
+func inputer(r io.Reader) <-chan string {
+	ch := make(chan string)
+	go func() {
+		s := bufio.NewScanner(r)
+		for s.Scan() {
+			ch <- s.Text()
+		}
+	}()
+	return ch
+}
+
+func oneSection() (str string) {
+	var (
+		ch_inp = inputer(os.Stdin)
+		to     = time.After(time.Second)
+	)
+oneWord:
+	for {
+		select {
+		case v := <-ch_inp:
+			str = keyboard.ConvertInputCode(v)
+			break oneWord
+		case <-to:
+			str = config.INTERVAL_LETTER
+			break oneWord
+		}
+	}
+	return
+}
+
+func oneWord() (str string) {
+	ch_inp := inputer(os.Stdin)
+	// strInput := make(chan string)
+
+	go func() {
+		select {
+		// case <-strInput:
+		case <-ch_inp:
+			str = "a"
+		}
+	}()
+
+	// ch_inp := inputer(os.Stdin)
+	// strInput <- <-ch_inp
+
+	if str != "" {
+		fmt.Print("inputed")
+		return
+	}
+
+	time.Sleep(time.Second)
+	str = "b"
+	return
+}
+
+func oneWord2() (str string) {
+	var ch_inp = inputer(os.Stdin)
+	to := make(chan bool)
+
+	// var to = time.After(5 * time.Second)
+	// word:
+	// time.NewTimer(5 * time.Second)
+	// to <- true
+
+	timer := time.NewTimer(5 * time.Second)
+	to <- true
+
+	for {
+		select {
+		case <-ch_inp:
+			str = "a"
+			timer.Stop()
+			return
+		case <-to:
+			str = "b"
+			return
+		}
+	}
 }
--- pkg/keyboard/keyboard.go
+++ pkg/keyboard/keyboard.go
@@ -1,190 +1,33 @@
 package keyboard
 
 import (
-	"errors"
-	"fmt"
-	"math/rand"
 	"morse/config"
-	"time"
-
-	"github.com/eiannone/keyboard"
 )
-
-const (
-	signal_single = "SINGLE"
-	signal_triple = "TRIPLE"
-	signal_quit   = "QUIT"
-)
-
-func InputEventOrIntervalFail() {
-	ticker := time.NewTicker(config.TYPING_INTERVAL * time.Millisecond)
-	defer ticker.Stop()
-
-	fin := make(chan bool)
-
-	go func() {
-		for {
-			select {
-			// case 入力があったら:
-			case <-fin:
-				return
-			case <-ticker.C:
-				print("_")
-			}
-		}
-	}()
-	time.Sleep(1 * time.Second)
-	fmt.Print("1")
-	time.Sleep(3 * time.Second)
-	fin <- true
-	fmt.Print("finish")
-}
-
-func InputEventOrInterval() string {
-	str := "_"
-	ping := make(chan bool)
-
-	go func() {
-		select {
-		case <-ping:
-			str = "a"
-		}
-	}()
-
-	// 仮のping送信
-	rand.Seed(time.Now().UnixNano())
-	if a := rand.Intn(2); a == 1 {
-		ping <- true
-	}
-
-	if str == "a" {
-		return str
-	}
-
-	time.Sleep(config.TYPING_INTERVAL * time.Millisecond)
-	return str
-}
-
-func InputOrInterval() (str string) {
-	ping := make(chan string)
-
-	char, _, err := keyboard.GetKey()
-	if err != nil {
-		panic(err)
-	}
-
-	go func() {
-		t := time.NewTimer(config.TYPING_INTERVAL * time.Millisecond)
-		<-t.C
-		ping <- config.INTERVAL_LETTER
-	}()
-
-	go func() {
-		for {
-			select {
-			case inp := <-ping:
-				if inp == config.SINGLE_PING {
-					str = config.SINGLE_LETTER
-				} else if inp == config.TRIPLE_PING {
-					str = config.TRIPLE_LETTER
-				} else if inp == config.QUIT_PING {
-					str = config.QUIT_LETTER
-				} else if inp == config.INTERVAL_LETTER {
-					fmt.Print("a")
-					str = config.INTERVAL_LETTER
-				}
-
-			}
-		}
-	}()
-
-	ping <- string(char)
-
-	return
-}
 
 //
-func MyGetKey(ret string) string {
-	if err := keyboard.Open(); err != nil {
-		panic(err)
-	}
-	defer keyboard.Close()
+// func MyGetKey(ret string) string {
+// 	if err := keyboard.Open(); err != nil {
+// 		panic(err)
+// 	}
+// 	defer keyboard.Close()
 
-	fmt.Println("Press L to quit")
-	for {
+// 	fmt.Println("Press L to quit")
+// 	for {
 
-		str := InputOrInterval()
+// 		str := InputOrInterval()
 
-		if str == config.QUIT_LETTER {
-			fmt.Println()
-			break
-		} else {
-			ret += str
-			fmt.Print(str)
-		}
-	}
-	return ret
-}
+// 		if str == config.QUIT_LETTER {
+// 			fmt.Println()
+// 			break
+// 		} else {
+// 			ret += str
+// 			fmt.Print(str)
+// 		}
+// 	}
+// 	return ret
+// }
 
-func getKey() (str string, err error) {
-	waitingForKey := make(chan bool)
-	inputComm := make(chan keyboard.KeyEvent)
-	// Check if opened
-	if !keyboard.IsStarted(time.Millisecond * 50) {
-		return "", errors.New("keyboard not opened")
-	}
-	// Check if already waiting for key
-	select {
-	case waitingForKey <- true:
-		return "", errors.New("already waiting for key")
-	default:
-	}
-
-	// 動いてない
-
-	for {
-		go func() {
-			select {
-			case ev := <-inputComm:
-				str = convertInputCode(string(ev.Rune))
-				print(str)
-				err = ev.Err
-
-			case keepAlive := <-waitingForKey:
-				if !keepAlive {
-					str = ""
-					err = errors.New("operation canceled")
-					print(err)
-				}
-			}
-		}()
-
-		if str != "" {
-			return
-		}
-
-		// こっちにしか来てない
-		time.Sleep(config.TYPING_INTERVAL * time.Millisecond)
-		str = config.INTERVAL_LETTER
-		return
-	}
-}
-
-func OriginalGetKey() (str string) {
-	rawStr, _ := getKey()
-	if rawStr == config.SINGLE_PING {
-		str = config.SINGLE_LETTER
-	} else if rawStr == config.TRIPLE_PING {
-		str = config.TRIPLE_LETTER
-	} else if rawStr == config.QUIT_PING {
-		str = config.QUIT_LETTER
-	} else if rawStr == config.INTERVAL_LETTER {
-		str = config.INTERVAL_LETTER
-	}
-	return
-}
-
-func convertInputCode(inp string) (out string) {
+func ConvertInputCode(inp string) (out string) {
 	if inp == config.SINGLE_PING {
 		out = config.SINGLE_LETTER
 	} else if inp == config.TRIPLE_PING {
@@ -193,6 +36,8 @@ func convertInputCode(inp string) (out string) {
 		out = config.QUIT_LETTER
 	} else if inp == config.INTERVAL_LETTER {
 		out = config.INTERVAL_LETTER
+	} else {
+		out = ""
 	}
 	return
 }
